f51f04e8ffff94141c2d19dc708ff66c8894d4fc
Simon Curty <simon.curty@students.unibe.ch>
2019-05-07 11:27:38 +0200

    fixes idx_to_class function

--- utils_labels.py
+++ utils_labels.py
@@ -1,10 +1,10 @@
 import json
 
 # assigns a book category (genre) to a number, might be useful for a simpler overview (optional?)
-def idx_to_class(class_name, dict):
-    for idx, name in dict.items():
-        if name == class_name:
-            return idx
+def idx_to_class(index, dict):
+    for name, idx in dict.items():
+        if idx == index:
+            return name
 
 def folder_to_cat_dict(path):
     with open('path', 'w') as outfile:
